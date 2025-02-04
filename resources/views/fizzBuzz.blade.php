92617a08e56a59f439e433e48d177cb5aeaf71dc
Damian Kilian <kilianpoczta@gmail.com>
2025-02-04 21:58:33 +0100

    scaffolding

--- resources/views/fizzBuzz.blade.php
+++ resources/views/fizzBuzz.blade.php
@@ -7,4 +7,6 @@
     </form>
 
     {{ $fizzBuzz }}
+    <hr>
+    
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\PersonController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
+Auth::routes();
+
 Route::get('/', [App\Http\Controllers\HomeController::class, 'fizzBuzz'])->name('fizzBuzz');
 
+Route::resource('persons', PersonController::class);
 
